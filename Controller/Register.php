0b98040280053aa7901a475a9e4278015a648c0f
Игорь <igorsux@mail.ru>
2015-04-05 23:54:16 +0300

    Добавила капчу

--- Controller/Register.php
+++ Controller/Register.php
@@ -5,6 +5,8 @@
  * Date: 05.12.14
  * Time: 16:41
  */
+
+session_start();
 class Controller_Register extends Controller_Base_View implements Controller_ControllerInterface
 {
     public function init()
@@ -26,27 +28,29 @@ class Controller_Register extends Controller_Base_View implements Controller_Con
         if ($_POST) {
             $date_users = new Data_Users();
             $date_users->add();
+            if(($_SESSION['secpic'] != null)&& ($_SESSION['secpic'] == strtolower($_POST['captch']))) {
+                $login = $_POST['login'];
+                $num_rows = $date_users->search($login);
+                if ($num_rows == 0) {
+                    if ($_POST['password1'] == $_POST['password2']) {
+                        $password = $_POST['password1'];
+                        $secondname = $_POST['secondname'];
+                        $email = $_POST['email'];
+                        $name = $_POST['name'];
+                        $sex = $_POST['sex'];
+                        if ($date_users->creteUser($login, $password, $secondname, $email, $name, $sex)) {
+                            echo 'Успешно добавлено';
+                        }
+                    } else {
+                        echo 'Параоли не савподают';
 
-            $login = $_POST['login'];
-            $num_rows = $date_users->search($login);
-            if ($num_rows == 0) {
-                if ($_POST['password1'] == $_POST['password2']) {
-                    $password = $_POST['password1'];
-                    $secondname = $_POST['secondname'];
-                    $email = $_POST['email'];
-                   // $name = $_POST['name'];
-                    $name= $_POST['name'];
-                    $sex = $_POST['sex'];
-                    if ($date_users->creteUser($login, $password, $secondname, $email, $name,$sex)) {
-                        echo 'Успешно добавлено';
                     }
                 } else {
-                    echo 'Параоли не савподают';
+                    echo 'Пользователь с таким именем уже существует!';
 
                 }
-            } else {
-                echo 'Пользователь с таким именем уже существует!';
-
+            }else{
+                echo "Вы не правы!";
             }
 
         }
