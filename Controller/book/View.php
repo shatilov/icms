63b9ab4cd830779c7c117bf8552ed435cb992f6f
Игорь <igorsux@mail.ru>
2015-05-26 17:42:18 +0300

    Добавил обработку пост запроса для добавления комментариев

--- Controller/book/View.php
+++ Controller/book/View.php
@@ -16,7 +16,24 @@ class Controller_book_View extends Controller_Base_View implements Controller_In
 	{
 		$this->init();
 		echo $this->display();
+        $this->comment();
 	}
+
+    public function comment(){
+        if ($_POST) {
+            if($_POST["comm"]){
+                $cmts = new Data_Comments();
+                $tex=$_POST["comm"];
+                $user = Data_CurrentUser::get();
+                $cmts->add($tex,$_GET['id'],$user->getId());
+                redirect('/?go=book&id=' . $_GET['id']);
+
+            }
+
+        }
+
+    }
+
     public static function  gl(){
         $user = Data_CurrentUser::get();
         echo  $user->getLogin();
