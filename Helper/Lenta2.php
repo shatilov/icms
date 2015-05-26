340b22ad7f54ca0cf14672563810cf731c717fc7
Игорь <igorsux@mail.ru>
2015-05-26 18:28:08 +0300

    Тот же файл только с цифрой 2 зато добавлися

--- Helper/Lenta2.php
+++ Helper/Lenta2.php
@@ -3,6 +3,57 @@
  * Created by PhpStorm.
  * User: Игорь
  * Date: 26.05.2015
- * Time: 18:25
+ * Time: 2:25
  */
-echo "asdgs ";
+
+class Helper_Lenta
+{
+
+    public static function getlenta($id_book)
+    {
+        $image = new Data_image();
+        $uss=new Data_Users();
+        $comments = new Data_Comments();
+        $c1 = $comments->get($id_book)->fetchAll();
+
+
+        $user = Data_CurrentUser::get();
+        if ($user) {
+            echo '<div class="comment" >
+					<div class="avamin">
+					    <img src="' . $image->get_avatar($user->getAvatar()) . '">
+
+							' . $user->getName() . '
+					</div>
+
+
+					<div class="txt" >
+					 <form name="test" method="post" action=" ">
+					 <textarea name ="comm" class="form-control" rows="3"></textarea>
+ 					</div>
+
+		 <button type="submit" class="btn btn-primary">Отправить</button>
+			<button type="reset" class="btn btn-inverse">очистить</button>
+				' . date("d-m-Y") . ' </form>
+				</div>';
+        }
+        if($c1) {
+            foreach ($c1 as $c) {
+                $links[] =
+                    '<div class="comment" >
+                            <div class="avamin">
+                                 <img src="' . $image->get_avatar($uss->get_ava($c["user_id"])) . '">
+                                    ' .$uss->get_name( $c["user_id"])  . '
+                                    </div>
+                            <div class="txt" > ' . $c["text"] .'</div>
+                                ' . $c["date"] . '
+                            </div>';
+
+            }
+            foreach ($links as $l) {
+                echo $l;
+            }
+        }
+    }
+
+}
